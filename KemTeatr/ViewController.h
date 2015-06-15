996db20288a69c3c847e3a4b5ce6b10b81b20e16
ascherbinin <ascherbinin@live.ru>
2015-06-15 10:04:43 +0700

    Initial Commit

---
+++ KemTeatr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KemTeatr
+//
+//  Created by Андрей Щербинин on 15.06.15.
+//  Copyright (c) 2015 Andrey Scherbinin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
